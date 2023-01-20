9e1aeb4272aaac20adf4fb260ac08020a674fe89
Denys <semikss1805fw@gmail.com>
2023-01-20 09:17:07 +0200

    PHFilter: image picker done

---
+++ PHFilter/PHFilter/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  PHFilter
+//
+//  Created by SemikSS on 19.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    @State private var showingImagePicker = false
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker, onDismiss: { showingImagePicker = false }) {
+            ImagePicker(image: $inputImage)
+        }
+        .onChange(of: inputImage) { newValue in
+            loadImage()
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PHFilter/PHFilter/PHFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHFilterApp.swift
+//  PHFilter
+//
+//  Created by SemikSS on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PHFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
