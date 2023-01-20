2131bac1d4661f7a66b12cd3b28e1cfe21358c36
Denys <semikss1805fw@gmail.com>
2023-01-20 15:55:08 +0200

    PHFilter: minimal functionality done

--- PHFilter/PHFilter/ContentView.swift
+++ PHFilter/PHFilter/ContentView.swift
@@ -5,44 +5,137 @@
 //  Created by SemikSS on 19.01.2023.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
+
+extension View {
+    func toastView(toast: Binding<FancyToast?>) -> some View {
+        self.modifier(FancyToastModifier(toast: toast))
+    }
+}
 
 struct ContentView: View {
     @State private var image: Image?
     @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
     @State private var showingImagePicker = false
+    
+    @State private var filterIntesity = 0.5
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    private let context = CIContext()
+    
+    @State private var showingFilterSheet = false
+    
+    @State private var toast: FancyToast? = nil
     
     var body: some View {
         VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
+            ZStack {
+                Rectangle()
+                    .fill(.secondary)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("Tap to select image")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                image?
+                    .resizable()
+                    .scaledToFit()
+            }
+            .onTapGesture {
                 showingImagePicker = true
             }
             
-            Button("Save Image") {
-                guard let inputImage = inputImage else { return }
-
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage
-                )
+            HStack {
+                Text("Intensity")
+                Slider(value: $filterIntesity)
+                    .onChange(of: filterIntesity) { _ in
+                        applyProcessing()
+                    }
+            }
+            
+            HStack {
+                Button("Change Filter") {
+                    showingFilterSheet = true
+                }
+                
+                Spacer()
+                
+                if image != nil {
+                    Button("Save") {
+                        saveImage()
+                    }
+                }
+                
             }
         }
+        .padding()
+        .padding(.top, 32)
+        .onChange(of: inputImage) { _ in loadImage() }
+        .toastView(toast: $toast)
         .sheet(isPresented: $showingImagePicker, onDismiss: { showingImagePicker = false }) {
             ImagePicker(image: $inputImage)
         }
-        .onChange(of: inputImage) { _ in
-            loadImage()
-            
-
+        .confirmationDialog("Select Filter", isPresented: $showingFilterSheet) {
+            Button("Crystallize") { setFilter(.crystallize()) }
+            Button("Bloom") { setFilter(.bloom()) }
+            Button("Gaussian  Blur") { setFilter(.gaussianBlur()) }
+            Button("Pixellate") { setFilter(.pixellate()) }
+            Button("Sepia Tone") { setFilter(.sepiaTone()) }
         }
     }
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func saveImage() {
+        guard let processedImage = processedImage else { return }
+
+        let imageSaver = ImageSaver()
+        
+        imageSaver.succesHandler = {
+            toast = FancyToast(type: .success, title: "Success", message: "Saving succeed")
+        }
+        
+        imageSaver.errorHandler = {
+            debugPrint($0)
+        }
+        
+        imageSaver.writeToPhotoAlbum(image: processedImage)
+    }
+    
+    func applyProcessing() {
+        let inputKays = currentFilter.inputKeys
+        
+        if inputKays.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntesity, forKey: kCIInputIntensityKey)
+        }
+        if inputKays.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterIntesity * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKays.contains(kCIInputScaleKey ) {
+            currentFilter.setValue(filterIntesity * 10, forKey: kCIInputScaleKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+         
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+            processedImage = uiImage
+        }
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 }
 
--- PHFilter/PHFilter/ImageSaver.swift
+++ PHFilter/PHFilter/ImageSaver.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class ImageSaver: NSObject {
+    var succesHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
     func writeToPhotoAlbum(image: UIImage) {
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
     }
     
-    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error, contextInfo: UnsafeRawPointer) {
-        debugPrint("Save finished")
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            succesHandler?()
+        }
     }
 }
